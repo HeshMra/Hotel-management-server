0890e26d70a3e62cf4500e367337b0d6548ba218
heshith <heshith619@gmail.com>
2024-10-08 14:38:34 +0530

    refactor

--- src/main/java/com/hotelmanagement/security/SpringSecurityProjectForHotelApplication.java
+++ src/main/java/com/hotelmanagement/security/SpringSecurityProjectForHotelApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.hotelmanagement.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

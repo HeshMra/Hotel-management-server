2c58cbb5f62ae5e9c0b5845e6824e52015dd3e88
heshith <heshith619@gmail.com>
2024-10-09 10:40:30 +0530

    user creation

--- src/main/java/com/example/demo/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/demo/config/ProjectSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -16,5 +18,10 @@ public class ProjectSecurityConfig {
                 .antMatchers("/api/v1/notice/my-notice").permitAll()
                 .and().formLogin().and().httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@CrossOrigin
+public class LoginController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @PostMapping("register")
+    public ResponseEntity<String>regsiterUser(@RequestBody User user){
+        ResponseEntity response = null;
+        try{
+            String hashPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(hashPassword);
+            User savedUser = userRepo.save(user);
+            if(savedUser.getId()>0){
+                response=ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given User Details Are Successfully registered");
+            }
+
+
+        }catch (Exception ex){
+            response=ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body("An Exception Occured due to"+ ex.getMessage());
+        }
+        return response;
+    }
+}

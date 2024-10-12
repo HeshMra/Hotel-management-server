f85b619622f167c15700cbb2ab9961970d942925
heshith <heshith619@gmail.com>
2024-10-12 12:10:23 +0530

    updated

--- src/main/java/com/example/demo/controller/InquiryController.java
+++ src/main/java/com/example/demo/controller/InquiryController.java
@@ -6,14 +6,12 @@ import com.example.demo.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/inquiry")
 
+@CrossOrigin(origins = "*")
 public class InquiryController {
     @Autowired
     private InquiryService inquiryService;
